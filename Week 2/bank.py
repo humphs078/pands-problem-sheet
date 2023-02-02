5bc48e1901a39766e0a850d1b6ee8c17b549e0be
humphs078 <123559682+humphs078@users.noreply.github.com>
2023-02-02 17:34:37 +0000

    Week 2 - Task

--- Week 2/bank.py
+++ Week 2/bank.py
@@ -2,12 +2,12 @@
 # Author: Sean Humphreys
 # Script for adding cents and putting Euro - Task 2
 
-a = int(input("Enter the first amount in cents ")) # Variable for amount 1 in cents and covert to integer
+a = int(input("Enter amount 1 in (cent) ")) # Variable for amount 1 in cents and covert to integer
 
-b = int(input("Enter the second amount in cents ")) # Variable for amount 2 in cents and convert to integer
+b = int(input("Enter amount 2 (in cent) ")) # Variable for amount 2 in cents and convert to integer
 
 c = float((a + b)/100) # variable to define the sum of the 2 amounts output and convert to decimal
 
-# print(f"The total amount is €{c}") # my original line of code was a problem as it prints €1.10 as €1.1 and not €1.10 as it should be
+# print(f"The sum of these is €{c}") # my original line of code was a problem as it prints €1.10 as €1.1 and not €1.10 as it should be
 
-print("The total amount is €""{:.2f}".format(c)) # Reference - http://programarcadegames.com/index.php?chapter=formatting&lang=en accessed 02/02/2023
+print("The total amount is €""{:.2f}".format(c)) # Fixes the issue encountered in the line above e.g. 1.1 prints as 1.10. Cause of issue was formatting. Solution found @ Reference - http://programarcadegames.com/index.php?chapter=formatting&lang=en accessed 02/02/2023. 
