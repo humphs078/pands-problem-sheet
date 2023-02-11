10c00b8b1428e88c171fa4db14cdb73d76687b16
humphs078 <sean@cruaghgreen.com>
2023-02-11 18:06:11 +0000

    Modidied Commentry

--- accounts.py
+++ accounts.py
@@ -2,8 +2,8 @@
 # Author: Sean Humphreys
 # Script to read in a bank account no', display only the last 4 digits and mask the rest
 
-# variable to input bank account no
+# variable to input bank account no and convert to string
 account_no = str(input("Please enter a 10 digit bank account number: "))
 
-# command to print mask 1st six digits and only print the last 4 digits
+# command to print "x" for 1st six digits and only print the last 4 digits
 print(f"xxxxxx{account_no[-4:]}")
---
+++ w3_accountsmodified.py
@@ -0,0 +1,5 @@
+# w3_accountsmodified.py
+# Author: Sean Humphreys
+# Script to read in a bank account no' of any length, display only the last 4 digits and mask the rest
+
+# variable to input bank account no
