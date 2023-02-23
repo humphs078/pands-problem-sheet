b4aaa6d21b9104e19bee148d03abb1f792d94f90
humphs078 <sean@cruaghgreen.com>
2023-02-23 07:56:24 +0000

    Changes to make commentary clearer.

--- Task_2_bank.py
+++ Task_2_bank.py
@@ -11,8 +11,8 @@ b = int(input("Enter amount 2 (in cent) "))
 # variable to define the sum of the 2 amounts output and convert to decimal
 c = float((a + b)/100)
 
-# print(f"The sum of these is €{c}") - this line of code was a problem as it prints €1.10 as €1.1 and not €1.10 as it
-# should be.
+# line 15 of code was a problem as it prints €1.10 as €1.1 and not €1.10 as it
+# print(f"The sum of these is €{c}")
 
 # Fixes the issue encountered in my original line of code above e.g. €1.1 prints as €1.10. Cause of issue was
 # formatting. Solution found @ Reference - http://programarcadegames.com/index.php?chapter=formatting&lang=en accessed
