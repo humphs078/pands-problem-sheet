ccdd89e54cae3e764f54bb3dfcb4a34f95496703
humphs078 <sean@cruaghgreen.com>
2023-02-23 07:46:03 +0000

    Commentary added to script. Reference added. Print statement changed to print string in bold.

--- Task_1_helloworld.py
+++ Task_1_helloworld.py
@@ -1,1 +1,7 @@
-print("hello world")
+# helloworld.py
+# Author: Sean Humphreys
+# Script to print 'Hello World!' string in bold
+# Reference - https://blog.finxter.com/how-to-print-bold-text-in-python/ last accessed 23/02/2023
+
+# This is a no library method to print the string in bold. "\" escape character is used to enclose the string
+print("\033[1m" + "Hello World!" + "\033[0m")
