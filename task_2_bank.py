8b215bff947cafdfe68ed11c7c46b3a6b63b92a1
humphs078 <sean@cruaghgreen.com>
2023-03-03 13:34:09 +0000

    Rename variables to meaningful names

--- task_2_bank.py
+++ task_2_bank.py
@@ -3,13 +3,13 @@
 # Script for adding cents and putting Euro - Task 2
 
 # Variable for amount 1 in cents and covert to integer
-a = int(input("Enter amount 1 in (cent) "))
+amount_1 = int(input("Enter amount 1 in (cent) "))
 
 # Variable for amount 2 in cents and convert to integer
-b = int(input("Enter amount 2 (in cent) "))
+amount_2 = int(input("Enter amount 2 (in cent) "))
 
 # variable to define the sum of the 2 amounts output and convert to decimal
-c = float((a + b)/100)
+euro_total = float((amount_1 + amount_2)/100)
 
 # line 15 of code was a problem as it prints €1.10 as €1.1 and not €1.10 as it
 # print(f"The sum of these is €{c}")
@@ -21,4 +21,4 @@ c = float((a + b)/100)
 
 # Line 20 was original solution - newer format string used below. Changed on 02/03/2023
 # Reference - https://java2blog.com/format-a-float-to-two-decimal-places/ accessed 02/03/2023
-print(f"The total amount is €{c:.2f}")
+print(f"The total amount is €{euro_total:.2f}")
