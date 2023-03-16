d864c65c623b01c78fcbbdca6a4078f20ee81c63
humphs078 <sean@cruaghgreen.com>
2023-03-16 16:15:05 +0000

    Week 8 task - commentry updated

--- task_8_plottask.py
+++ task_8_plottask.py
@@ -4,6 +4,7 @@
 # a histogram of a normal distribution of 1000 values with a mean of 5 and standard deviation of 2,
 # and a plot of the function  h(x)=x3 in the range [0, 10],
 # on the one set of axes.
+# Reference 1:
 import numpy as np  # import the numpy module as np
 from numpy import random  # from the numpy package import the random module
 import matplotlib.pyplot as plt  # import the matplotlib.pyplot as plt
@@ -26,15 +27,16 @@ def plot_func():
 
 for x in range(0, 10):  # for loop to create a list of values for the x-axis
     x_axis_list.append(x)  # append the desired values in a list for the x-axis
-# Define a dictionary with key value pairs
+# Define a dictionary with key value pairs for font formatting
 font1 = {'family': 'serif', 'color': '#6495ED', 'size': 20}
+# Define a dictionary with key value pairs for font formatting
 font2 = {'family': 'serif', 'color': '#9932CC', 'size': 10}
-x_axis = np.array(x_axis_list)
-y_axis = np.array(plot_func())
-plt.hist(random_numbers, color="#A9A9A9")
-plt.plot(x_axis, y_axis, color='#006400', linewidth='2')
-plt.grid(color='#8A2BE2', linewidth="0.1")
-plt.title("Week 8 Task - plottask.py", fontdict=font1)
-plt.xlabel("Number", fontdict=font2)
-plt.ylabel("Number Cube", fontdict=font2)
-plt.show()
+x_axis = np.array(x_axis_list)  # variable with array for x-axis
+y_axis = np.array(plot_func())  # variable with array for y-axis
+plt.hist(random_numbers, color="#A9A9A9")  # plot histogram with grey colour
+plt.plot(x_axis, y_axis, color='#006400', linewidth='2')  # plot line with green colour and width 2
+plt.grid(color='#8A2BE2', linewidth="0.1")  # plot a black light blue with line width 0.1
+plt.title("Week 8 Task - plottask.py", fontdict=font1)  # display title with font 1 formatting
+plt.xlabel("Number", fontdict=font2)  # display x-axis label with font 2 formatting
+plt.ylabel("Number Cube", fontdict=font2)  # display y-axis with font 2 formatting
+plt.show()  # command to show plot
